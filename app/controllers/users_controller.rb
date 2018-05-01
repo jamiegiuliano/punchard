bee0157518a191b20545171bf12a09e55cfbe163
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-01 08:46:25 -0700

    User can now sign up and view their account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: session[:user_id])
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :phone_number, :password)
   end
 end
