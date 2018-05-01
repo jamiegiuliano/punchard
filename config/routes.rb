084b55e67f9d7f6ae23ba552da7931a28cb46c49
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-01 15:03:01 -0700

    Changed to namespace route to clean up unnecessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static#welcome"
 
-  resources :users do
+  namespace :users do
     resources :merchants, only: [:new, :create]
   end
-
-  # Login/Logout routes
-  get '/login', to: 'session#new'
-  post '/session', to: 'session#create'
-  get '/logout', to: 'session#destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
