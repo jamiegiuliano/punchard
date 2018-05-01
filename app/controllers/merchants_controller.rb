b45b49c68eadfc6f26e10ad9c806b8ed15da4b14
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-01 09:50:21 -0700

    Add ability to create a merchant and link at same time.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,9 @@
 class MerchantsController < ApplicationController
+  def new
+    @merchant = Merchant.new(user_id: session[:user_id])
+  end
+
+  def create
+    raise params.inspect
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
   belongs_to :user
   has_one :link
+
+  accepts_nested_attributes_for :link
 end
