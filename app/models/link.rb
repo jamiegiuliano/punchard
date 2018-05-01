284bcf1109c3ef925337e456bf478037a85bc451
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-01 10:54:00 -0700

    Add validations to user and link. Created custom validation in link model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,12 @@
 class Link < ApplicationRecord
   belongs_to :merchant
+
+  validates :url, presence: true
+  validate :url_from_square
+
+  def url_from_square
+    if !url.include?("squareup.com/")
+      errors.add(:url, "Must be full link from squareup.com")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :merchants
   has_many :links, through: :merchants
+
+  validates :email, presence: true, uniqueness: true, format: /@/
+  validates :password, presence: true, length: { in: 5..10 }
 end
