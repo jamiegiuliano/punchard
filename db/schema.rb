69c2b0ff8edf540e7a06f13ac34f08f84747a574
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-09 13:59:24 -0700

    Add image column to Merchant table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_155301) do
+ActiveRecord::Schema.define(version: 2018_05_09_205756) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_155301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "star_count"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
