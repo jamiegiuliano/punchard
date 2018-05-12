0812a525219781d6c90de2f9159f23a1a90c5ce2
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-11 21:50:31 -0700

    Issue with Heroku images.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
 
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
   config.assets.digest = true
   # Settings specified here will take precedence over those in config/application.rb.
 
