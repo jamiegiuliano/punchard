be3af861d30f9c0845305e2b1412a11ccb90d9f2
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-14 11:16:18 -0700

    Omniauth not working in Heroku.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
+    {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
 
   # ==> Warden configuration
