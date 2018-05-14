4f825f044535df6d6b8fa53262ae924d9e4771fe
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-14 11:29:02 -0700

    Syntax error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
 
   # ==> Warden configuration
