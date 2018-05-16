5e048b55aae6194ec319a855aed57d29bc01e16a
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-16 16:24:28 -0700

    Remove param argument in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } 
 
-  resources :users, only: [], param: :user_id do
+  resources :users, only: [] do
     member do
       resources :merchants
       get '/current_favorite', to: 'merchants#favorite'
