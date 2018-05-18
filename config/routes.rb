f80e9a19d1c75f5dcfdd2e06a34360a4dab26f1c
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-18 14:10:12 -0700

    Fix route user id param name and adding link to show page on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: [] do
+  resources :users, only: [], param: :user_id do
     member do
       resources :merchants
       get '/current_favorite', to: 'merchants#favorite'
