509439282ebbea0790c49a379b9cbcb88cd41c6c
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-18 11:55:58 -0700

    Fix routesto display correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } 
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: [] do
-    member do
+  resources :user, only: [] do
       resources :merchants
       get '/current_favorite', to: 'merchants#favorite'
-    end
   end
 
   authenticated :user do
