b65fb3b66cfba5b86af963d5d7e7be81d6fe9550
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-05-21 09:19:23 -0700

    Fix route error

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :users, only: [], param: :user_id do
-    member do
       resources :merchants
       get '/current_favorite', to: 'merchants#favorite'
-    end
   end
 
   authenticated :user do
