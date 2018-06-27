b64b3d69a19f8135be84a08d42ef5b211cfce9a3
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-06-27 16:26:28 -0700

    Generate Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_205756) do
+ActiveRecord::Schema.define(version: 2018_06_27_230446) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "url"
