18a673165ce63e891dbfdbbd3b90d935eba13fd3
jamiegiuliano <jamiepgiuliano@gmail.com>
2018-07-11 15:44:35 -0700

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
 
   # Mount Action Cable outside main process or domain
