3bef37ae17ea8b3b13a9984c8a0f655dbf46a7bc
jamiegiuliano <jamiepgiuliano@gmail.com>
2018-07-12 10:41:51 -0700

    Add License

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -6,7 +6,7 @@ function Link(data) {
 }
 
 Link.prototype.buildLinks = function() {
-  var output = `<li class="collection-item"><a href="${this.url}" target="_blank" hidden_field="${this.id}">${this.category.name}</a>
+  let output = `<li class="collection-item"><a href="${this.url}" target="_blank" hidden_field="${this.id}">${this.category.name}</a>
                   <a href="/merchants/${this.merchant_id}/links/${this.id}/edit"><i class="tiny material-icons">edit</i></a>
                   <a data-confirm="Are you sure?" data-method="delete" href="/merchants/${this.merchant_id}/links/${this.id}"><i class="tiny material-icons">delete_forever</i></a>`;
   return output;
